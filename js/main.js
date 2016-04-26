0a395846e9d4996a330e585a37dced4a04e281f4
Vincetic, Martin <martin.vincetic@gmail.com>
2016-04-26 17:16:14 +0200

    js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
+// Retrieve
+var MongoClient = require('mongodb').MongoClient;
 
+// Connect to the db
+MongoClient.connect("mongodb://localhost:27017/local", function(err, db) {
+  if(!err) {
+    console.log("We are connected");
+  }
+});
