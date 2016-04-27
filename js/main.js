89d4521c9a5e8c112d325c47ec7cd29286a4c114
Vincetic, Martin <martin.vincetic@gmail.com>
2016-04-27 15:38:18 +0200

    js-json-index.html

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
-// Retrieve
-var MongoClient = require('mongod').MongoClient;
+$(document).ready(function() {
 
-// Connect to the db
-MongoClient.connect("mongodb://localhost:27017/local", function(err, db) {
-  if(!err) {
-    console.log("We are connected");
-  }
+  $.getJSON("../json/getContent.json", function(data) {
+    alert(data);
+    var output ="<p>";
+    for (var i in data.contents){
+      output += data.contents[i].content;
+    }
+    output+="</p>";
+    document.getElementById("contentInstallation").innerHTML=output;
+
+  });
 });
