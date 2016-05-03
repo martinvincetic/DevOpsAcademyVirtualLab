31212708e0a42bdbd30617db374937b557d4010f
Vincetic, Martin <martin.vincetic@gmail.com>
2016-05-03 12:22:08 +0200

    a

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
   $(".testButton2").on("click", function() {
 
-    $.getJSON('http://172.17.0.2:8888/YT/rest/channels/getChannelList', function( data ) {
+    $.getJSON('http://127.0.0.1:8888/YT/rest/channels/getChannelList', function( data ) {
        $.each(data, function(index, element) {
            $("#channelId").append("<option value=\"" + element.EcomOutletId + "\">" + element.channelName + "</option>");
        });
