3fec582e6e22e3015a25b08ccbb7a4d8978a6e94
Vincetic, Martin <martin.vincetic@gmail.com>
2016-05-03 14:57:00 +0200

    callback json

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
 $(document).ready(function() {
+  /*
+      $(".testButton2").on("click", function() {
 
-  $(".testButton2").on("click", function() {
-
-    $.getJSON('http://127.0.0.1:8888/YT/rest/channels/getChannelList', function( data ) {
-       $.each(data, function(index, element) {
-           $("#channelId").append("<option value=\"" + element.EcomOutletId + "\">" + element.channelName + "</option>");
+        $.get('http://127.0.0.1:8888/YT/rest/channels/getChannelList?callback=parseResponse', function( data ) {
+           $.each(data, function(index, element) {
+               $("#channelId").append("<option value=\"" + element.EcomOutletId + "\">" + element.channelName + "</option>");
+           });
        });
-   });
 
+     });*/
+  $(".testButton2").on("click", function() {
+    var url = 'http://127.0.0.1:8888/YT/rest/channels/getChannelList?callback=parseResponse';
+    var xhr = createCORSRequest('GET', url);
+    xhr.send();
   });
-
 });
